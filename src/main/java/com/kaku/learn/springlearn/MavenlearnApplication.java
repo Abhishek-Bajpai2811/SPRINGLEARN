84d903ac497bbb7b437a403d06dfe8caefc6c20b
Abhishek Bajpai <abajpai2811@gmail.com>
2024-09-08 15:03:08 +0530

    intial commit

---
+++ src/main/java/com/kaku/learn/springlearn/MavenlearnApplication.java
@@ -0,0 +1,13 @@
+package com.kaku.learn.springlearn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MavenlearnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MavenlearnApplication.class, args);
+	}
+
+}
